efe256144a7b4b2bb5a4eca978ac5768b83d9b3c
Niklas Åkerlund <akerlund.niklas@gmail.com>
2017-02-28 19:47:39 +0100

    MachineID is now singleton.

--- src/main/app/MachineID.java
+++ src/main/app/MachineID.java
@@ -5,9 +5,10 @@ import java.net.NetworkInterface;
 
 public class MachineID {
 
+    private static MachineID instance = null;
     private byte[] mID;
 
-    public MachineID() {
+    private MachineID() {
         try {
             InetAddress ip = InetAddress.getLocalHost();
             NetworkInterface ni = NetworkInterface.getByInetAddress(ip);
@@ -16,6 +17,12 @@ public class MachineID {
         catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static MachineID getInstance() {
+        if(instance == null)
+            instance = new MachineID();
+        return instance;
     }
 
     public byte[] getMachineID() {
