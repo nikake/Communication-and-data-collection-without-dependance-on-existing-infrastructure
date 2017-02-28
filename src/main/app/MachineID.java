a1926548e70d3e0eebab8127efe1b16fa4fd97ad
Niklas Åkerlund <akerlund.niklas@gmail.com>
2017-02-28 19:22:04 +0100

    MachineID can now be created when initialising MachineID

--- src/main/app/MachineID.java
+++ src/main/app/MachineID.java
@@ -1,6 +1,24 @@
 package main.app;
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+
 public class MachineID {
 
+    private byte[] mID;
 
+    public MachineID() {
+        try {
+            InetAddress ip = InetAddress.getLocalHost();
+            NetworkInterface ni = NetworkInterface.getByInetAddress(ip);
+            mID = ni.getHardwareAddress();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public byte[] getMachineID() {
+        return mID;
+    }
 }
