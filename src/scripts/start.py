b045d0ee1b815be5db573332e1635e8f454e2acd
Niklas Åkerlund <akerlund.niklas@gmail.com>
2017-03-13 09:50:25 +0100

    Updated comments

--- src/scripts/start.py
+++ src/scripts/start.py
@@ -17,19 +17,19 @@ applicationPath = ""
 outputPath = os.getcwd()+"/./"
 outputChannel = open(os.devnull, 'w')
 
-// apply super user access
+""" apply super user access"""
 subprocess.call("sudo su", shell=True, stdout=outputChannel, stderr=subprocess.STDOUT)
 
-// On each node, bring the wireless interface down, change the device's operating mode and SSID, then raise the interface.
+""" On each node, bring the wireless interface down, change the device's operating mode and SSID, then raise the interface."""
 subprocess.call("ifconfig wlan0 down", shell=True, stdout=outputChannel, stderr=subprocess.STDOUT)
 subprocess.call("iwconfig wlan0 channel 1 essid MYNETWORK mode ad-hoc", shell=True, stdout=outputChannel, stderr=subprocess.STDOUT)
 subprocess.call("ifconfig wlan0 up", shell=True, stdout=outputChannel, stderr=subprocess.STDOUT)
 
-// Scan for ad-hoc cells in range (necessary for some drivers to trigger IBSS scanning)
+""" Scan for ad-hoc cells in range (necessary for some drivers to trigger IBSS scanning) """
 subprocess.call("iwlist wlan0 scan", shell=True, stdout=outputChannel, stderr=subprocess.STDOUT)
 
-// Assign Unique IP to the node
+""" Assign Unique IP to the node """
 subprocess.call("ifconfig wlan0 " + ipAddress + " netmask 255.255.255.0", shell=True, stdout=outputChannel, stderr=subprocess.STDOUT)
 
-// Start Java application
-// TODO
+""" Start Java application """
+""" TODO """
