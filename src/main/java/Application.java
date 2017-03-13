c219d55152523ad7095d43bd89d20a1a0eed812d
Niklas Åkerlund <akerlund.niklas@gmail.com>
2017-03-13 14:36:31 +0100

    Built test program to test DeviceScanner in action.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,8 +1,30 @@
 package main.java;
 
+import main.java.network.DeviceScanner;
+
 /**
  * The main class of the application.
  */
 public class Application {
 
+    private static boolean keepRunning = true;
+
+    public static void main(String[] args) {
+
+        while(keepRunning) {
+            DeviceScanner ds = DeviceScanner.getInstance();
+            Thread scan = new Thread(ds);
+            try {
+                scan.start();
+                scan.join();
+            } catch (InterruptedException e) {
+                System.out.println("Scan was interrupted.");
+            }
+            ds.getDevices();
+        }
+    }
+
+    public void shutdown() {
+        keepRunning = false;
+    }
 }
