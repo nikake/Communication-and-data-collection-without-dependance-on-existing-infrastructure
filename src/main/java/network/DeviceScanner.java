d43ee182ea6d516f8330ccbc637450c71f0c812a
Niklas Åkerlund <akerlund.niklas@gmail.com>
2017-03-13 11:26:57 +0100

    Added DeviceScanner.java

---
+++ src/main/java/network/DeviceScanner.java
@@ -0,0 +1,7 @@
+package main.java.network;
+
+/**
+ * Created by Niklas on 2017-03-13.
+ */
+public class DeviceScanner {
+}
---
+++ src/test/java/network/DeviceScannerTest.java
@@ -0,0 +1,16 @@
+package test.java.network;
+
+import main.java.network.DeviceScanner;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class DeviceScannerTest {
+
+    DeviceScanner deviceScanner;
+
+    @Before
+    public void init() {
+        deviceScanner = new DeviceScanner();
+    }
+
+}
