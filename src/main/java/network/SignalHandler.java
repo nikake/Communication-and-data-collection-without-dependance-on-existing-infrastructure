7345098eab171629a78b0cb683e048b3c2b90d36
Niklas Åkerlund <akerlund.niklas@gmail.com>
2017-04-09 21:26:13 +0200

    SignalHandler now uses CommandExecutor to execute its scan command.

--- src/main/java/network/SignalHandler.java
+++ src/main/java/network/SignalHandler.java
@@ -1,11 +1,8 @@
 package main.java.network;
 
 import main.java.log.Logger;
+import main.java.util.CommandExecutor;
 import main.java.util.Device;
-import main.java.util.MachineID;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 
 public class SignalHandler {
 
@@ -16,39 +13,20 @@ public class SignalHandler {
     };
     private final Device device;
     private final String iNetAddress;
-    private Logger log;
+    private Logger log = Logger.getLogger(this.getClass().getSimpleName());
 
     public SignalHandler(Device device, String iNetAddress) {
         this.device = device;
         this.iNetAddress = iNetAddress;
-        log = Logger.getLogger(this.getClass().getSimpleName());
+
     }
 
-    private String scan() {
-        StringBuilder data = new StringBuilder();
-        try {
-            System.out.println("Executing commands: " + SCAN_COMMAND[2]);
-            Process p = Runtime.getRuntime().exec(SCAN_COMMAND);
-            p.waitFor();
-            BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
-            BufferedReader error = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-            String line;
-            while((line = br.readLine().trim()) != null) {
-                System.out.println("Appending: " + line);
-                data.append(line + "\n");
-            }
-            while((line = error.readLine()) != null) {
-                System.out.println("Error: " + line);
-            }
-            System.out.println("Scan complete. Length: " + data.length());
-        } catch(Exception e) {
-            System.out.println(e);
-        }
-        return data.toString();
+    private String[] scan() {
+        return CommandExecutor.execute(SCAN_COMMAND);
     }
 
     public int getSignalStrength() {
-        String[] data = scan().split("\n");
+        String[] data = scan();
         for(int i = 0; i < data.length - 3; i++) {
             System.out.println("[" + data[i] + "]");
             if(data[i].equals("ESSID:\"RPiAdHocNetwork\"")) {
