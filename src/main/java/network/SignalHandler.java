7954b7ca8be9449f4618b97fce38a50534828c58
Niklas Åkerlund <akerlund.niklas@gmail.com>
2017-04-09 15:54:55 +0200

    Build construct for SignalHandler to accept a MachineID and an iNetAddress

--- src/main/java/network/SignalHandler.java
+++ src/main/java/network/SignalHandler.java
@@ -1,5 +1,14 @@
 package main.java.network;
 
+import main.java.util.MachineID;
+
 public class SignalHandler {
 
+    private final MachineID device;
+    private final String iNetAddress;
+
+    public SignalHandler(MachineID device, String iNetAddress) {
+        this.device = device;
+        this.iNetAddress = iNetAddress;
+    }
 }
