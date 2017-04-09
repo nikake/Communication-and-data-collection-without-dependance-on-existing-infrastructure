5b48a7effb8928a9287d3d8d6f3dc7dc2b5d25ef
Niklas Åkerlund <akerlund.niklas@gmail.com>
2017-04-09 16:18:30 +0200

    Added private function scan() which returns a String with data from the executed command SCAN_COMMAND.

--- src/main/java/network/SignalHandler.java
+++ src/main/java/network/SignalHandler.java
@@ -2,13 +2,33 @@ package main.java.network;
 
 import main.java.util.MachineID;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class SignalHandler {
 
+    private final static String SCAN_COMMAND = "iwlist wlan0 scan | egrep 'Address|ESSID|Quality|Signal'";
     private final MachineID device;
     private final String iNetAddress;
 
     public SignalHandler(MachineID device, String iNetAddress) {
         this.device = device;
         this.iNetAddress = iNetAddress;
+    }
+
+    private String scan() {
+        StringBuilder data = new StringBuilder();
+        try {
+            Process p = Runtime.getRuntime().exec(SCAN_COMMAND);
+            p.waitFor();
+            BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line;
+            while((line = br.readLine()) != null) {
+                data.append(line + "\n");
+            }
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+        return data.toString();
     }
 }
