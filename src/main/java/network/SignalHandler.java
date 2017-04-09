10debea6d08a85b290f9f9559064332f50be1fcf
Niklas Åkerlund <akerlund.niklas@gmail.com>
2017-04-09 21:13:43 +0200

    SignalHandler can now parse info about found devices giving information such as signal strength

--- src/main/java/network/SignalHandler.java
+++ src/main/java/network/SignalHandler.java
@@ -1,5 +1,7 @@
 package main.java.network;
 
+import main.java.log.Logger;
+import main.java.util.Device;
 import main.java.util.MachineID;
 
 import java.io.BufferedReader;
@@ -7,28 +9,54 @@ import java.io.InputStreamReader;
 
 public class SignalHandler {
 
-    private final static String SCAN_COMMAND = "iwlist wlan0 scan | egrep 'Address|ESSID|Quality|Signal'";
-    private final MachineID device;
+    private final static String[] SCAN_COMMAND = {
+            "/bin/sh",
+            "-c",
+            "iwlist wlan0 scan | egrep 'Address|ESSID|Quality|Signal'"
+    };
+    private final Device device;
     private final String iNetAddress;
+    private Logger log;
 
-    public SignalHandler(MachineID device, String iNetAddress) {
+    public SignalHandler(Device device, String iNetAddress) {
         this.device = device;
         this.iNetAddress = iNetAddress;
+        log = Logger.getLogger(this.getClass().getSimpleName());
     }
 
     private String scan() {
         StringBuilder data = new StringBuilder();
         try {
+            System.out.println("Executing commands: " + SCAN_COMMAND[2]);
             Process p = Runtime.getRuntime().exec(SCAN_COMMAND);
             p.waitFor();
             BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            BufferedReader error = new BufferedReader(new InputStreamReader(p.getErrorStream()));
             String line;
-            while((line = br.readLine()) != null) {
+            while((line = br.readLine().trim()) != null) {
+                System.out.println("Appending: " + line);
                 data.append(line + "\n");
             }
+            while((line = error.readLine()) != null) {
+                System.out.println("Error: " + line);
+            }
+            System.out.println("Scan complete. Length: " + data.length());
         } catch(Exception e) {
             System.out.println(e);
         }
         return data.toString();
+    }
+
+    public int getSignalStrength() {
+        String[] data = scan().split("\n");
+        for(int i = 0; i < data.length - 3; i++) {
+            System.out.println("[" + data[i] + "]");
+            if(data[i].equals("ESSID:\"RPiAdHocNetwork\"")) {
+                log.info(data[i]);
+                log.info(data[i+1]);
+                log.info(data[i+2]);
+            }
+        }
+        return 0;
     }
 }
