f9e5791ac3af928f8e8d9c3ffcf2ae0e1d752942
Niklas Åkerlund <akerlund.niklas@gmail.com>
2017-04-09 21:48:59 +0200

    Fixed bug with .trim() when reading lines from the BufferedReader

--- src/main/java/util/CommandExecutor.java
+++ src/main/java/util/CommandExecutor.java
@@ -19,30 +19,33 @@ public class CommandExecutor {
         try {
             Process p = Runtime.getRuntime().exec(commands);
             p.waitFor();
-            data = readData(new BufferedReader(new InputStreamReader(p.getInputStream())));
-            errors = readErrors(new BufferedReader(new InputStreamReader(p.getErrorStream())));
+            data = readData(p);
+            errors = readErrors(p);
             if(!errors.isEmpty())
                 log.error(errors);
         } catch(Exception e) {
-            System.out.println(e);
+            e.printStackTrace();
         }
         return data.split("\n");
     }
 
-    private static String readData(BufferedReader br) throws IOException {
+    private static String readData(Process p) throws IOException {
         StringBuilder data = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
         String line;
-        while((line = br.readLine().trim()) != null) {
-            data.append(line + "\n");
+        while((line = br.readLine()) != null) {
+            System.out.println("Adding line: " + line.trim());
+            data.append(line.trim() + "\n");
         }
         return data.toString();
     }
 
-    private static String readErrors(BufferedReader br) throws IOException {
+    private static String readErrors(Process p) throws IOException {
         StringBuilder data = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(p.getErrorStream()));
         String line;
-        while((line = br.readLine().trim()) != null) {
-            data.append(line + "\n");
+        while((line = br.readLine()) != null) {
+            data.append(line.trim() + "\n");
         }
         return data.toString();
     }
