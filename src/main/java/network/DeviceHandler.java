6ae25c0f022b99a9ff53eb8c927f9725b50825e7
filip.lindvall <filiplindvall21@hotmail.com>
2017-04-22 21:54:08 +0200

    Added DeviceHandler class

---
+++ src/main/java/network/DeviceHandler.java
@@ -0,0 +1,27 @@
+package network;
+
+import main.java.util.Device;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+
+public class DeviceHandler implements Runnable {
+
+    private static DeviceHandler instance = null;
+    private ConcurrentHashMap<String, Device> devices = new ConcurrentHashMap<String, Device>();
+
+    private DeviceHandler() {
+
+    }
+
+    public static DeviceHandler getInstance() {
+        if (instance == null)
+            instance = new DeviceHandler();
+        return instance;
+    }
+
+    public void run(){
+        //// TODO: 2017-04-22
+    }
+
+}
