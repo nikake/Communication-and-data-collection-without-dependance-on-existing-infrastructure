16cb66051fc4b751b45582d9a3024f8375bc8ce3
filip.lindvall <filiplindvall21@hotmail.com>
2017-04-26 17:19:15 +0200

    BluetoothScanner is now runnable

--- src/main/java/network/BluetoothScanner.java
+++ src/main/java/network/BluetoothScanner.java
@@ -3,24 +3,18 @@ package main.java.network;
 import main.java.util.CommandExecutor;
 import main.java.util.Device;
 
-public class BluetoothScanner {
+public class BluetoothScanner implements Runnable {
 
+    private static BluetoothScanner instance = new BluetoothScanner();
     private CommandExecutor commandExecutor = new CommandExecutor();
     private static final String CMD = "python bluetooth_rssi.py";
 
-    public double scanDevice(Device device){
-        // Get 10 rssi values
-        double[] rssiValues = new double[10];
-        for(double d : rssiValues){
-            d = Double.parseDouble(commandExecutor.execute(CMD + " " + device.btAddress.split("\\s+"))[0]);
-        }
-        // Get the average of the 10 rssi values
-        double sum = 0;
-        for(double d : rssiValues){
-            sum += d;
-        }
-
-        return sum / rssiValues.length;
+    public BluetoothScanner getInstance(){
+        return instance;
     }
 
+    @Override
+    public void run() {
+
+    }
 }
