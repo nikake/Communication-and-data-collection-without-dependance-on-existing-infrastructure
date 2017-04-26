d6e8f38c6639c34d87cb6b1b6ec5e8d86afa11b4
Niklas Åkerlund <akerlund.niklas@gmail.com>
2017-04-26 14:17:57 +0200

    DeviceScanner should now run scan() within 50-60ms instead of every 5 seconds.

--- src/main/java/network/DeviceScanner.java
+++ src/main/java/network/DeviceScanner.java
@@ -1,41 +1,51 @@
 package main.java.network;
 
 import main.java.Application;
+import main.java.log.Logger;
+
 import java.net.InetAddress;
 import java.util.ArrayList;
+import java.util.concurrent.*;
 
 public class DeviceScanner {
-
-    private final int MAX_TIMEOUT = 50;
+    private final static int MAX_CLIENTS = 256;
+    private final static int MAX_TIMEOUT = 50;
     private ArrayList<String> foundDevices;
+    private ArrayList<InetAddress> addresses;
+    private final static Executor POOL = Executors.newFixedThreadPool(MAX_CLIENTS);
 
     public DeviceScanner() {
-
+        foundDevices = new ArrayList<>();
+        addresses = new ArrayList<>();
+        initAddresses();
     }
 
-    /*
-        Scan for devices every 5 seconds and add them to the collection 'devices'.
-        This should be refactored and changed to future....in the future.
-     */
-    public ArrayList<String> scan() {
-        foundDevices = new ArrayList<>();
+    private void initAddresses() {
         String[] subnet = Application.getInstance().getHostAddress();
-
         for (int i = 0; i < 256; i++) {
             subnet[3] = Integer.toString(i);
             try {
                 InetAddress addr = InetAddress.getByName(subnet[0] + "." + subnet[1] + "." + subnet[2] + "." + subnet[3]);
-                if (!foundDevices.contains(i) && !subnet[3].equals(Application.getInstance().getHostAddress()[3]) && addr.isReachable(MAX_TIMEOUT)){
-                    foundDevices.add(addr.getHostAddress());
-                }
-
+                if(!subnet[3].equals(Application.getInstance().getHostAddress()[3]))
+                    addresses.add(addr);
             } catch (Exception e) {
                 System.out.println(e);
             }
         }
-
-        return foundDevices;
-
     }
 
+    /*
+        Scan for devices every 50 (minimum) milliseconds and add them to the collection 'foundDevices' if it has not already been discovered and is reachable.
+     */
+    public ArrayList<String> scan() {
+        addresses.parallelStream().forEach(addr -> {
+            try {
+                if (!foundDevices.contains(addr) && addr.isReachable(MAX_TIMEOUT))
+                    foundDevices.add(addr.toString());
+            } catch (Exception e) {
+                Logger.error("Exception while adding device with IP " + addr + " to foundDevices while scanning. Exception: " + e.getMessage());
+            }
+        });
+        return foundDevices;
+    }
 }
