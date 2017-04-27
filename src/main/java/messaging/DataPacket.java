a639bd60b1d2bd97d742fe4d60b171b1f65300c8
Niklas Åkerlund <akerlund.niklas@gmail.com>
2017-04-27 16:54:56 +0200

    DataPacket now consists of a sender, a receiver, a message and a routing table

--- src/main/java/messaging/DataPacket.java
+++ src/main/java/messaging/DataPacket.java
@@ -1,5 +1,19 @@
 package main.java.messaging;
 
+import main.java.network.RoutingTable;
+import main.java.util.Device;
+
 public class DataPacket {
 
+    public final Device SENDER;
+    public final Device RECEIVER;
+    public final Message MESSAGE;
+    public final RoutingTable ROUTING_TABLE;
+
+    public DataPacket(Device sender, Device receiver, Message msg, RoutingTable rTable) {
+        this.SENDER= sender;
+        this.RECEIVER = receiver;
+        this.MESSAGE = msg;
+        this.ROUTING_TABLE = rTable;
+    }
 }
