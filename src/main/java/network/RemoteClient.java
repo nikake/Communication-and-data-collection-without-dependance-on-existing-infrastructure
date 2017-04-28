63e53c7d19bbe8858664ebf9798f1478d3b70429
Niklas Åkerlund <akerlund.niklas@gmail.com>
2017-04-28 17:23:45 +0200

    Refactored RemoteClient

--- src/main/java/network/RemoteClient.java
+++ src/main/java/network/RemoteClient.java
@@ -4,20 +4,37 @@ import main.java.Application;
 import main.java.log.Logger;
 import main.java.util.Device;
 
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 public class RemoteClient implements Runnable {
 
-    private String hostIp;
-    private int hostPort;
+    private Socket host;
     private Device hostDevice = null;
+    private ObjectOutputStream hostWriter = null;
+    private ObjectInputStream hostReader = null;
+
+    private String ip;
+    private int port;
 
 
-    public RemoteClient(String hostIp, int hostPort) {
-        this.hostIp = hostIp;
-        this.hostPort = hostPort;
+    public RemoteClient(String ip, int port) {
+        this.ip = ip;
+        this.port = port;
+    }
+
+    private void connectToHost() throws IOException {
+        host = new Socket(ip, port);
+        Logger.info("Connected to remote host with IP: " + host.getRemoteSocketAddress());
+        System.out.println("Connected to remote host with IP: " + host.getRemoteSocketAddress());
+
+    }
+
+    private void initiateStreams() throws IOException {
+        hostWriter = new ObjectOutputStream(host.getOutputStream());
+        hostReader = new ObjectInputStream(host.getInputStream());
     }
 
     public Device getHostDevice() {
@@ -32,33 +49,42 @@ public class RemoteClient implements Runnable {
         return hostDevice != null ? hostDevice: Application.NULL_DEVICE;
     }
 
+    private void setHostDevice() {
+        do {
+            try {
+                hostDevice = (Device) hostReader.readObject();
+            } catch (Exception e) {
+                Logger.info("Attempted to set clientDevice, but failed.\n\n" + e.getMessage());
+            }
+        } while (hostDevice == null);
+        Logger.info("Communication with new device established. New device: " + hostDevice);
+        System.out.println("Communication with new device established. New device: " + hostDevice);
+    }
+
+    private void readHostMessages() throws IOException, ClassNotFoundException {
+        Object message = null;
+        while((message = hostReader.readObject()) != null) {
+
+        }
+    }
+
+    private void close() throws IOException {
+        if (host != null)
+            host.close();
+    }
+
     @Override
     public void run() {
-        Socket host = null;
         try {
-            host = new Socket(hostIp, hostPort);
-            Logger.info("Connected to remote host with IP: " + host.getRemoteSocketAddress());
-            System.out.println("Connected to remote host with IP: " + host.getRemoteSocketAddress());
-            ObjectInputStream ois = new ObjectInputStream(host.getInputStream());
-            do {
-                try {
-                    hostDevice = (Device) ois.readObject();
-                } catch (Exception e) {
-                    Logger.info("Attempted to set clientDevice, but failed.\n\n" + e.getMessage());
-                }
-            } while (hostDevice == null);
-            Logger.info("Communication with new device established. New device: " + hostDevice);
-            System.out.println("Communication with new device established. New device: " + hostDevice);
-
-            while(true) {
-                Thread.sleep(1000);
-            }
+            connectToHost();
+            initiateStreams();
+            setHostDevice();
+            readHostMessages();
         } catch (Exception e) {
-            Logger.error("Error during connection to remote host with IP: " + hostIp + "\n\n" + e.getMessage());
+            Logger.error("Error during connection to remote host with IP: " + ip + "\n\n" + e.getMessage());
         } finally {
             try {
-                if (host != null)
-                    host.close();
+                close();
             } catch (Exception e) {
                 Logger.error("Error closing connection to remote host with IP: " + host.getRemoteSocketAddress());
             }
